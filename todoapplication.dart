b415a17ab7809e3d54fda85d3a368988fa6af075
ShroogJA <nshroog03@gmail.com>
2023-09-13 16:00:33 -0600

    modifire

--- todoapplication.dart
+++ todoapplication.dart
@@ -1,53 +1,123 @@
 import 'dart:convert';
 import 'dart:io';
+ File f = new File("birthdays.json");
+  String content =  f.readAsStringSync();
+  List<Map<String, dynamic>> jsonContent = jsonDecode(content).cast<Map<String, dynamic>>();
+main()  {
 
-main() async {
-  File f = new File("informaiction.json");
-  String content = await f.readAsStringSync();
-  List<Map<String, dynamic>> file = [];
-  print("Welcome To To-Do List System");
+ print("====================================================");
+ print("Welcome To To-Do List System");
+ 
+ print("====================================================");
+  
   print("1.Add Task");
   print("2.Remove Task");
   print("3.View Task");
   print("4.Filter Tasks based on Title");
+   print("====================================================");
 
-  var opperation;
+  var opperation='';
   do {
     stdout.write("choose number :");
      opperation = stdin.readLineSync()!;
      switch (opperation) {
-       case 1 :
-       stdout.write("Enter proparty: ");
-        String name = stdin.readLineSync()!;
-        stdout.write("Enter proparty: ");
-        String newtask = stdin.readLineSync()!;
-
-        Map<String, dynamic> newline = {'name': newtask, 'birthday': name};
-          file.add(newline);
-
-  File fi = File('informaiction.json');
-  fi.writeAsStringSync(jsonDecode(content));
-      //   file.forEach((element) {
-      // String task = element['name'].toString().toLowerCase();
-      // String proparty = element['proparty'].toString().toLowerCase();
-
-      // if (name.contains(searchName)) {
-      //   print("${element['name']}'s birthday is ${element['birthday']}");
-      
-      // }
-    // });
-         
+         case '1':
+         addTask();
          break;
-         case 2:
-         
+         case '2':
+         removeTask();
          break;
-         case 3:
-         
+         case' 3':
+         viewTask();
          break;
-         case 4:
-         
+         case '4':
+         filterTask();
          break;
        default:
      }
   } while (opperation != 0);
 }
+  
+void addTask(){
+  stdout.write("Enter task title : ");
+  String title =stdin.readLineSync()!;
+
+  stdout.write("Enter task description : ");
+  String description = stdin.readLineSync()!;
+
+
+Map<String, dynamic> newline = {'title': title, 'description': description};
+jsonContent.add(newline);
+
+File file = File("birthdays.json");
+file.writeAsStringSync(jsonEncode(jsonContent));
+  
+  print("Added Successfully");
+  print("====================================================");
+}
+
+void removeTask(){
+  if (jsonContent.isEmpty){
+    print("there is no any task!");
+    print("====================================================");
+
+    return;
+  }
+ 
+  
+  viewTask();
+
+  stdout.write("Enter the title to remove it :");
+
+  String? title = stdin.readLineSync();
+      print(title);
+  if (jsonContent.removeAt(jsonContent) => jsonContent){
+    jsonContent.remove(title);
+    
+    print("Removed $title Successfully");
+    print("====================================================");
+
+
+  }else{
+    print("input Erorr!!");
+    print("====================================================");
+
+  }
+
+  }
+
+void viewTask(){
+  if (jsonContent.isEmpty){
+    print("====================================================");
+
+    print("there is no any task!");
+    print("====================================================");
+
+    return;
+  }
+  else{
+   print("====================================================");
+
+   print("tasks:");
+
+   print(jsonContent);
+
+print("====================================================");
+  }
+} 
+void filterTask(){
+ stdout.write("Filter the List if the Title Contains: ");
+ String nameFilter = stdin.readLineSync()!;
+ 
+
+     nameFilter = stdin.readLineSync()!;
+  for (var i in jsonContent){
+  if((i["title"]).toLowerCase().contains(nameFilter)){
+    print("${i["title"]} : ${i["description"]}");
+  }
+  
+
+    } 
+  print("====================================================");
+
+  }
